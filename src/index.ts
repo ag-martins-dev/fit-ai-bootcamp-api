7db220d9c294cf2378fcb34866442571da290c7e
Alison Gabriel <odevalison@gmail.com>
2026-03-01 18:13:22 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-const name = "";
+import "dotenv/config";
+
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { hello: "true" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 8080 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
